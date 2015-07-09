601559c94641c3631f5aa29a4495fe80998178fb
Evan Ogas <evanogas@gmail.com>
2015-07-08 19:27:37 -0500

    Autogenerate all new days, not just for sample stack

--- CalorieStack/Controllers/HomeController.cs
+++ CalorieStack/Controllers/HomeController.cs
@@ -24,21 +24,10 @@ namespace CalorieStack.Controllers
 
             var day = context.Days.FirstOrDefault(d => d.StackId == id && d.Date == DateTime.Today);
             
-            // If the sample Stack doesn't have a Day for today, delete all other sample Days,
-            // then create a new Day for today
-            if (id == "sample" && day == null)
+            if (day == null)
             {
-                // TODO: Figure out how to do this better with cascading deletes
-                var sampleDays = context.Days.Where(d => d.StackId == "sample");
-                var sampleMeals = context.Meals.Where(m => sampleDays.Any(d => d.Id == m.DayId));
-                var sampleFoodItems = context.FoodItems.Where(fi => sampleMeals.Any(m => fi.MealId == m.Id));
-
-                context.FoodItems.RemoveRange(sampleFoodItems);
-                context.Meals.RemoveRange(sampleMeals);
-                context.Days.RemoveRange(sampleDays);
-
-                day = context.Days.Add(Day.CreateSample());
-
+                // Create the current Day if it is missing
+                day = context.Days.Add(Day.CreateDefault());
                 context.SaveChanges();
             }
 
